57abed41bb8ed3a92b2a22479fba1548abe54a0d
Bharat Sharma <82795831+Bharatsharma19@users.noreply.github.com>
2023-03-11 09:25:42 +0530

    Week 8

--- Week-8/Pattern1.java
+++ Week-8/Pattern1.java
@@ -4,18 +4,20 @@ public class Pattern1 {
     public static void main(String[] args) {
         Scanner inr = new Scanner(System.in);
         int n = inr.nextInt();
-        int a15 = 0, SuM = 0;
-        for (int z = 1; z <= n; ++z, a15 = 0) {
-            for (int space = 1; space <= n - z; ++space) {
+
+        int temp = 0, Sum = 0;
+
+        for (int i = 1; i <= n; ++i, temp = 0) {
+            for (int space = 1; space <= (n - i); ++space) {
                 System.out.print("  ");
             }
-            while (a15 != 2 * z - 1) {
+            while (temp != (2 * i - 1)) {
                 System.out.print("* ");
-                SuM += 1;
-                ++a15;
+                Sum += 1;
+                ++temp;
             }
             System.out.println();
         }
-        System.out.print(SuM);
+        System.out.print(Sum);
     }
 }
